7c084d65c0ea9b5550995282f52375af8ac93bd0
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-17 02:46:21 +0100

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) /* main.c */
+/*
+ * main entry point
+ * return always zero
+ */
+int main(void) 
 {
 	char *s = "\"Programming is like building a multilingual puzzle";
 
