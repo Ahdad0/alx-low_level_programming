b2dbe8667f38ab869b94c7024623dfcf29f049ca
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-18 00:51:19 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all alpphabet expect q and e 
+ * Return: 0 (success)
+ */
+int main(void) 
+{
+	int i;
+	
+	for(i = 97; i <= 122; i++)
+	{
+		if(i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
