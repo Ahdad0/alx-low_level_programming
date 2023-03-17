fcd2bf950efef87cb8504b4d0649d6312dff5a5b
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-17 02:13:22 +0100

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
-int main(void)
+int main(void) /*the main function doesn't take any of the arguments*/
 {
 	char *s = "\"Programming is like building a multilingual puzzle\n";
 	puts(s);   /* print *s of the variable */
-	return (0);
+	return (0); 
 }
