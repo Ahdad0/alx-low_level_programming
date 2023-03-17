c297b0825ae9a5120a0910b39fe4e15c0a9c58cf
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-18 00:45:37 +0100

    last digit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase then uppercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter <= 'Z') 
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
