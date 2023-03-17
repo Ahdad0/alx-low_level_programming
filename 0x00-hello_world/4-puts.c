6be8a7c77be63af4abfec0ef6ed2fb86f04985a5
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-17 02:29:12 +0100

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main(void) /*adding void to not return the value*/
+int main(void) // adding void to not return the value
 {
 	char *s = "\"Programming is like building a multilingual puzzle\n";
 	puts(s);
+	return (0);
 }
