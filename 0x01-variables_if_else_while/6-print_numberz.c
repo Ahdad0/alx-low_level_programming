3fe79d96f6d74a7695d23ea4155e6c6c2f6b7081
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-18 01:03:06 +0100

    print all number based 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all single digit based 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
