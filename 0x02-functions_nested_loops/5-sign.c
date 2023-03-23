04bc15c25f2758df5bae37f795dc90f84df821eb
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-23 16:54:29 +0000

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print + or - when condition is true
+ * @n: an integer
+ * Return: 0 always
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
