b292c3c3a9ae7dd93b22d3b0192f0e7dd77a53ae
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-23 17:49:57 +0000

    computes the value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: an integer
+ * Return: -num if less 0, num if greater than 0
+ */
+int _abs(int)
+{
+	int num;
+
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else if (num > 0)
+	{
+		return (num);
+	}
+}
