9129098dd6a0db21ebae5fcc8544d5ec3359a0f3
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-29 21:05:17 +0000

    Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print first 98 Fibonacci number starting with 1 and 2
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a = 1, b = 2, next;
+
+	for (int i = 3; i <= 98; i++)
+	{
+		next = a + b;
+		printf("%d", next);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
+}
