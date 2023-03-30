d0d0a2c9d5f6cf53196a584b21883f861653592e
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-30 21:47:05 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,66 +4,38 @@
  * main - replace multiple of 3 and 5 with letter
  * Return: 0 always
  */
-int main (void)
+int main(void)
 {
 	int a;
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a < 10)
+		if (a % 3 == 0)
 		{
-			if (a % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else if (a % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else if (a % 3 == 0 && a % 5 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-			else
-			{
-				_putchar(a + '0');
-				_putchar(' ');
-			}
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a < 10)
+		{
+			_putchar(a + '0');
+			_putchar(' ');
 		}
 		else if (a < 100)
 		{
-			if (a % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else if (a % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else if (a % 3 == 0 && a % 5 == 0)
-			{
-				printf("FizzBuzz ")
-			}
-			else
-			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-				putchar(' ');
-			}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(' ');
 		}
 		else if (a == 100)
 		{
-			if (a % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				_putchar((a / 100) + '0');
-				_putchar(((a / 10) % 10) + '0');
-				_putchar((a % 10) + '0');
-				_putchar(' ');
-			}
+			printf("Buzz ");
 		}
 	}
 	_putchar('\n');
