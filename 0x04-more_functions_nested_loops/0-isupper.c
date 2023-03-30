de626ae64f7e67e66d74770fdf8f9c65634e5ddd
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-30 09:37:33 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - checks if integer is uppercase
+ * @c: alphabet
+ * Return: 1 if uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+	int u;
+
+	for (u = 65; u <= 90; u++)
+	{
+		if (u == c)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+}
