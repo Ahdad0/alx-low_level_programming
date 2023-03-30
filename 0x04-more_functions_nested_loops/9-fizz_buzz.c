140277f6fef1d0d08c7d3cfc35617a261f1c0f39
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-30 21:22:59 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - replace multiple of 3 and 5 with letter
+ */
+int main (void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a < 10)
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (a % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else if (a % 3 == 0 && a % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				_putchar(a + '0');
+				_putchar(' ');
+			}
+		}
+		else if (a < 100)
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (a % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else if (a % 3 == 0 && a % 5 == 0)
+			{
+				printf("FizzBuzz ")
+			}
+			else
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+				putchar(' ');
+			}
+		}
+		else if (a == 100)
+		{
+			if (a % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				_putchar((a / 100) + '0');
+				_putchar(((a / 10) % 10) + '0');
+				_putchar((a % 10) + '0');
+				_putchar(' ');
+			}
+		}
+	}
+}
