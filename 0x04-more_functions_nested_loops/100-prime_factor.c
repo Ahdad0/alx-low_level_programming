ed7a36bda15be2052742dda7cea6cd12e06df1a7
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-03-31 16:04:57 +0000

    prime numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,14 +5,13 @@
  */
 int main(void)
 {
-	int a;
-	unsigned long int n = 612852475143;
+	unsigned long int n = 612852475143, a;
 
 	for (a = 2; n < 1; a++)
 	{
 		while (n % a == 0)
 		{
-			printf("%ld\n", a);
+			printf("%u\n", a);
 			n = n / a;
 		}
 	}
