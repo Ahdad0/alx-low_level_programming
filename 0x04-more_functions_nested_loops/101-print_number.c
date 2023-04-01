c25be4356379739e6f5e5600c0d07e2dc4967291
AHD4D <118624944+Ahdad0@users.noreply.github.com>
2023-04-01 01:11:16 +0000

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,16 @@ void print_number(int n)
 
 	if (n < 0)
 	{
+		m = -n;
 		_putchar('-');
-		n1 = -n;
 	}
 	else
 	{
-		n1 = n;
+		m = n;
 	}
-	if (n1 / 10)
+	if (m / 10)
 	{
-		print_number(n1 / 10);
+		print_number(m / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar((m % 10) + '0');
 }
