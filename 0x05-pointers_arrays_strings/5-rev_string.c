9e1f81edef382e443390760605d836eef44a70e9
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-04 00:52:29 +0000

    all Done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverse the string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int len = 0, i;
+	char sum;
+
+	while (*s != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		sum = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = sum;
+	}
+}
