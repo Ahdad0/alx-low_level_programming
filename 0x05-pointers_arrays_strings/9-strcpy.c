8fe8c0c6968dc9067885da0e96715d718096de13
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-05 00:03:36 +0000

    test

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - copies the sting include ('\0') in dest
+ * @dest: string
+ * @src: string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
