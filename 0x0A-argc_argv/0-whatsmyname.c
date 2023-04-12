ce7e0ceabbe7b72fcea41f7ff856017c3f985747
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-12 18:28:07 +0000

    all Done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - print program name.
  * @argc: argument count.
@@ -7,6 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	_putchar(argv[0]);
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
