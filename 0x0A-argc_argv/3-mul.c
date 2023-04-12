f194add1217eab9a220182f9f664befe0c68e373
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-12 21:02:04 +0000

    all Done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -35,15 +35,23 @@ int main(int argc, char *argv[])
 {
 	int i, count = 0, coount = 0, sum = 0;
 
-	for (i = 1; i < argc - 1; i++)
+	if (argc < 2)
 	{
-		count += _atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
-	for (i = 2; i < argc; i++)
+	else if (argc > 2)
 	{
-		coount += _atoi(argv[i]);
+		for (i = 1; i < argc - 1; i++)
+		{
+			count += _atoi(argv[i]);
+		}
+		for (i = 2; i < argc; i++)
+		{
+			coount += _atoi(argv[i]);
+		}
+		sum = count * coount;
+		printf("%d\n", sum);
 	}
-	sum = count * coount;
-	printf("%d\n", sum);
 	return (0);
 }
