30bbbb05bc52ae5d5d1a8dbfe6a861865ce37abe
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-12 16:23:20 +0000

    all Done

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - searche a string for any of a set of cha.
+ * @s: string to be searched.
+ * @accept: set of cha to search for.
+ * Return: s or NULL.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, c, len = 0, len2 = 0;
+
+	while (accept[len])
+	{
+		len++;
+	}
+	while (s[len2])
+	{
+		len2++;
+	}
+	for (c = 0; c < len2; c++)
+	{
+		for (i = 0; i < len; i++)
+		{
+			if (accept[i] == s[c])
+			{
+				return (s + c);
+			}
+		}
+	}
+	return (NULL);
+}
