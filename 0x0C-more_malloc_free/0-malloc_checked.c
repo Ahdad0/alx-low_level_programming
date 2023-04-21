e84f5f0b9b6279b7be5b6cef5628919dd5476ab9
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-21 21:26:40 +0000

    all Done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * malloc_checked - function
@@ -13,9 +14,7 @@ void *malloc_checked(unsigned int b)
 	ar = malloc(b);
 
 	if (!ar)
-	{
 		exit(98);
-	}
 
 	return (ar);
 }
