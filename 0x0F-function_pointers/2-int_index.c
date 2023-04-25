72c1b4b9d63592cde41c7472551de01375b8e548
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-04-25 16:12:30 +0000

    all Done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function
+ * @array: array name
+ * @size: number of elements in the array
+ * @cmp: pointer to the function
+ * Return: sum or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, sum = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (sum);
+
+		sum++;
+	}
+
+	return (-1);
+}
