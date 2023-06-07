346414c648133fe5c1ed979d7f74d08e3e833845
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-06-07 16:35:49 +0100

    all Done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,15 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else if (*s == '\0')
+	{
+		putchar(*s);
+		putchar('\n');
+	}
+
 }
