72e8f897792eabf3110c00f799cb67191b1e2fbf
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-06-17 21:04:00 +0100

    reallocate memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_realloc - function sthat allocate a memory block
+ * @ptr: memory previously allocated
+ * @old_size: size
+ * @new_size: size
+ * Return: NULL or p or ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		return (NULL);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		return (p);
+	}
+	free(ptr);
+	p = malloc(old_size + new_size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	return (p);
+}
