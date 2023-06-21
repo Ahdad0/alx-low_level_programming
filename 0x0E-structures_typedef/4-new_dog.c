c006cd1b38009945fc5466627e83c595e2d67849
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-06-21 18:27:02 +0100

    Done

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strdu - copie string
+ * @s: string
+ * Return: ar or NULL
+ */
+char *_strdu(char *s)
+{
+	int i = 0;
+	int len = 0;
+	char *ar;
+
+	if (!s)
+		return (NULL);
+
+	while (s[len])
+		len++;
+
+	ar = malloc(sizeof(char) * (len + 1));
+
+	if (!ar)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		ar[i] = s[i];
+
+	ar[i] = '\0';
+
+	return (ar);
+}
+
+/**
+ * new_dog - function
+ * @name: value
+ * @age: value
+ * @owner: value
+ * Return: NULL or new
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = malloc(sizeof(dog_t));
+
+	if (!new)
+		return (NULL);
+
+	new->name = _strdu(name);
+
+	if (!new->name)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->age = age;
+	new->owner = _strdu(owner);
+
+	if (!new->owner)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
+
+	return (new);
+}
