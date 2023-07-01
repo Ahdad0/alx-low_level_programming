188e76d8341b0ac19f2475ca3e5e1e9a0c42ab56
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-07-01 01:49:51 +0100

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,26 @@
  */
 size_t print_list(const list_t *h)
 {
+	list_t *dup;
 	size_t count = 0;
 
 	if (h->str == NULL)
 	{
-		h->len = 0;
-		h->str = "(nil)";
+		dup = malloc(sizeof(list_t));
+
+		dup->str = "(nil)";
+		dup->len = 0;
+
+		printf("[%d] %s\n", dup->len, dup->str);
+		count++;
+
+		free(dup);
 	}
-	printf("[%d] %s\n", h->len, h->str);
-	count++;
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		count++;
+	}
 
 	h = h->next;
 
