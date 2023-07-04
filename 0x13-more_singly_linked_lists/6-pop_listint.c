c88f67690462caaa42ea7d1947957fe7ea4ea4d1
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-07-04 20:35:30 +0100

    delete head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,9 +11,17 @@ int pop_listint(listint_t **head)
 	int value;
 	listint_t *ptr;
 
+	if (!*head)
+	{
+		return (0);
+	}
+
 	ptr = *head;
+
 	value = (*head)->n;
 	*head = (*head)->next;
+
 	free(ptr);
+
 	return (value);
 }
