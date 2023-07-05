7a84e7f77c46237a10325b5f61ccffd9cd31bcf0
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-07-06 00:01:24 +0100

    return the sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n)
+ * @head: head
+ *
+ * Return: sum or 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
