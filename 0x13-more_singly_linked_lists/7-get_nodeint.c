8f2c6eb55775e6d0545fa42e6098fb86fe473c21
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-07-05 23:41:34 +0100

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -38,9 +38,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	ptr = head;
 
-	if ((listint_len(head) < index) || (head == NULL) || (index < 0))
+	if (listint_len(head) < index)
 	{
 		return (0);
+	}
+
+	if (head == NULL)
+	{
+		return (NULL);
 	}
 	else
 	{
