9e29b6e28baefaeea7a0779842d7103d0999cf96
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-07-05 23:49:06 +0100

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- * listint_len - return len of the nodes
- * @h: head
- *
- * Return: len
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *ptr;
-	size_t count = 0;
-
-	ptr = h;
-
-	while (ptr != NULL)
-	{
-		count++;
-		ptr = ptr->next;
-	}
-
-	return (count);
-}
-
-/**
  * get_nodeint_at_index - returns the nth node
  * @head: header
  * @index: num
@@ -32,43 +10,18 @@ size_t listint_len(const listint_t *h)
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr;
-	listint_t *add;
-	unsigned int i = 0, data = 0;
+	unsigned int i = 0;
 
-	ptr = head;
-
-	if (head == NULL)
+	while (i < index)
 	{
-		return (NULL);
-	}
-	else
-	{
-		while (i <= index)
-		{
-			if (i == index)
-			{
-				data = ptr->n;
-			}
-			ptr = ptr->next;
-			if (ptr->next == NULL)
-			{
-				return (NULL);
-			}
-			i++;
-		}
-
-		add = malloc(sizeof(listint_t));
-
-		if (ptr == NULL)
+		if (!(head->next))
 		{
 			return (NULL);
 		}
 
-		add->n = data;
-		add->next = head;
-		head = add;
-
-		return (add);
+		head = head->next;
+		i++;
 	}
+
+	return (head);
 }
