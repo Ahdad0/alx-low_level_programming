dbd10ec4d150b9d4bfd77f03ee7020f145d6d8f7
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-07-09 18:01:11 +0100

    done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary string to an unsigned int.
+ * @b: A pointer to the binary string.
+ *
+ * Return: 0 or total
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int total;
+
+	total = 0;
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		total = (total << 1) + (b[i] - '0');
+	}
+
+	return (total);
+}
