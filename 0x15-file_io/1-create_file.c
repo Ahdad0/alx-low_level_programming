9effaf7881a58c5c8d166acafc197f7864b0a144
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-08-07 19:40:02 -0400

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 or 0 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int succ;
+	int len = 0;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	succ = write(file, text_content, len);
+	if (succ == -1)
+		return (-1);
+
+	close(file);
+	return (1);
+}
