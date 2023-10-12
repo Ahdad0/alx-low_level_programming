e22f99b6106db8b36f46e91c8fb2df24f3166a4a
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-12 15:36:04 +0100

    handle NULL head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node = NULL;
 	dlistint_t *check = NULL;
 
+	if (!(*head))
+		return (NULL);
+
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 	{
