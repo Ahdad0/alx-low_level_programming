6ac003a4d50b15b96f5e3e053c1f170039b750e6
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-12 21:19:06 +0100

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,45 @@
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - inserts a node at a given index
+ *
+ * @h: double pointer to the list
+ * @idx: index of the node to insert
+ * @n: data to insert
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *front = *h, *back = *h, *insert = NULL;
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (!(*h))
+	dlistint_t *new;
+	dlistint_t *temp = *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new || !h)
 		return (NULL);
-	insert = malloc(sizeof(dlistint_t));
-	if (insert == NULL)
-		return (NULL);
-	while (front != NULL)
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (idx != 0)
-			i++;
-		if (i == idx)
+		if (i == idx - 1)
 		{
-			if (front->next != NULL)
-				front = front->next;
-			break;
-		}
-		back = back->next;
-		front = front->next;
-	}
-	insert->n = n;
-	if (idx != 0)
-	{
-		back->next = insert;
-		insert->prev = back;
-		if (front != NULL)
-		{
-			insert->next = front;
-			front->prev = insert;
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
+			new->next = temp->next;
+			new->prev = temp;
+			temp->next->prev = new;
+			temp->next = new;
+			return (new);
 		}
 		else
-			insert->next = NULL;
+			temp = temp->next;
 	}
-	else if (idx == 0)
-	{
-		*h = insert;
-		insert->prev = NULL;
-		insert->next = back;
-		back->prev = insert;
-		back->next = front;
-	}
-	return (insert);
+	return (NULL);
 }
