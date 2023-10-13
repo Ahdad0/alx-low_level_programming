99aa78778c0cdf18a144ff9830ae450115e8fcd0
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-13 19:41:25 +0100

    task 8 done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,21 +19,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (temp == NULL)
 			return (-1);
+
 		temp = temp->next;
 	}
 	if (temp == *head)
 	{
 		*head = temp->next;
+
 		if (*head != NULL)
 			(*head)->prev = NULL;
 	}
 	else
 	{
 		temp->prev->next = temp->next;
+
 		if (temp->next != NULL)
 			temp->next->prev = temp->prev;
 	}
+
 	free(temp);
+
 	return (1);
-}
 }
