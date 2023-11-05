1b133dca2a997ab94b1acba86357631e5bb66ceb
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-05 17:06:03 +0100

    fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new->next = ht->array[i];
+	ht->array[i] = new;
 	return (1);
 }
